5317d737b28a37edce0ddb3495f63daba6bd6bb7
Ross Gardler <ross.gardler@microsoft.com>
2022-03-20 23:09:03 -0700

    Don't attach the camera to the player
    Makes me dizzy!

---
+++ Assets/_MinDiab/Scripts/Runtime/UI/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WizardsCode.MinDiab.UI
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The object that the camera should follow.")]
+        Transform m_Target;
+
+        void Update()
+        {
+            transform.position = m_Target.position;
+        }
+    }
+}
